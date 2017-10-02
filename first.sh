ee6382a83c2268f0f08177adc28ba3d05a67c15a
jpartain89 <jpartain89@gmail.com>
2017-10-02 06:06:40 -0500

    Removed all the crap

--- first.sh
+++ first.sh
@@ -9,12 +9,12 @@
 # TODO: For the linux_dotfiles, remove the "non-dotfiles" files from the repo
 
 if [[ $(uname) == "Darwin" ]]; then
-    USER_HOME=/Users/jpartain89
+    USER_HOME="/Users/${USER}"
     GIT_DIR="$USER_HOME/git"
     FIRST_STEPS="$GIT_DIR/macos-first-steps"
 fi
 
-WORKDIR=$(pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ -e "$(which allunix)" ]]; then ALLUNIX=$(which allunix); fi
     if [[ "$ALLUNIX" ]]; then . "$ALLUNIX"
     else
@@ -24,28 +24,21 @@ if [[ -e "$(which allunix)" ]]; then ALLUNIX=$(which allunix); fi
         exit 1;
     fi
 
-
 macos_pre () {
     if [[ $(xcode-select --install 2>/dev/null) == "1" ]] ; then
-        echo ""
-        echo "Looks like you already installed xcode's CLI stuff"
-        echo "Press Enter to Continue..."
-        echo ""
+cat <<- EOF
+
+    Looks like you already installed xcode's CLI stuff
+    Press Enter to Continue...
+
+EOF
         read -r
     fi
-    if [[ $(which brew) == "" ]]; then
+    if [[ $(comand -v brew) == "" ]]; then
         echo ""
-        echo "Homebrew is not installed yet. Fixing that"
+        echo "Now installing HomeBrew."
         echo ""
         /usr/bin/ruby -e "$(curl -fsSL  https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        echo "Running brew doctor"
-         /usr/local/bin/brew doctor
-        echo "Press Enter to Continue "
-        read -r
-        echo "Running brew update"
-        /usr/local/bin/brew update
-        echo "Press Enter to Continue"
-        read -r
     fi
 }
 
@@ -53,21 +46,7 @@ install_brew_apps () {
     echo ""
     echo "Now installing basic Homebrew Stuffs"
     /usr/local/bin/brew install git --with-brewed-curl --with-persistent-https
-    /usr/local/bin/brew install python --with-sphinx-doc
-    /usr/local/bin/brew install python3 --with-sphinx-doc
-    /usr/local/bin/brew tap rcmdnk/file
     /usr/local/bin/brew install rcmdnk/file/brew-file
-}
-
-install_git_apps () {
-    if [[ $(which -s git) == "1" ]]; then
-        echo ""
-        echo "Git is not installed."
-        echo "Probably a lot other items missing."
-        echo "Fixing."
-        echo ""
-        sudo apt-get install git git-core python python-pip make apt-transport-https libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python-dev build-essential python2.7 python3.5 python3.5-pip python-setuptools unzip
-    fi
 }
 
 dotfile_clone () {
@@ -83,13 +62,12 @@ dotfile_clone () {
         echo ""
         echo "Dotfiles git repo is not cloned locally, fixing."
         git clone https://jpartain89@github.com/jpartain89/dotfiles.git "$GIT_DIR/dotfiles"
+        if [[ -x "${GIT_DIR}/dotfiles/linking" ]]; then
+            echo ""
+            echo "Running the linking script"
+            bash -ex "${GIT_DIR}/dotfiles/linking"
+        fi
     fi
-
-    for iPIP in pip pip3 pip3.5 pip3.6;
-    do
-        sudo -H "$iPIP" install --upgrade pip setuptools wheel
-        sudo -H "$iPIP" install --upgrade -r "$FIRST_STEPS/requirements.txt"
-    done
 }
 
 macos_pre && install_brew_apps; dotfile_clone; bash ./git_bootstrap.sh
